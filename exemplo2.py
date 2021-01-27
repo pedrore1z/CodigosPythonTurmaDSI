b45522f445084dcac24539be025cec6e8b534838
Pedro Gabriel <pedroreis1122@hotmail.com>
2021-01-27 15:46:12 -0300

    Criado arquivo exemplo 2

---
+++ exemplo2.py
@@ -0,0 +1,16 @@
+# operadores + - * / %
+# operador de exponenciação **
+'''
+Será comentário de várias linhas
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+
+print(2**num1)
+'''
+
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+media = (nota1 + nota2)/2
+print("A media entre as notas {} e {} é {}".format(nota1, nota2, media))
