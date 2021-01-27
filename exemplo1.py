6c8a78c90cf025fcd531c3601d76a9e92a18433f
Pedro Gabriel <pedroreis1122@hotmail.com>
2021-01-27 14:52:40 -0300

    Criado arquivo inicial em python

---
+++ exemplo1.py
@@ -0,0 +1,5 @@
+nome = input("\n\nQual o seu nome? ")
+endereco = input("Qual seu endereço? ")
+idade = input("Qual sua idade? ")
+print("\n\nOlá o seu nome é ",nome, "voce mora no endereço", endereco, "e tem ",idade,"anos\n\n")
+
